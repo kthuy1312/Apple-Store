7d61385b73f1894634e2fa444fc97e2962bb1b6e
kthuy1312 <lythuy13122004@gmail.com>
2026-04-27 00:37:53 +0700

    feat(router, layout): setup router và hoàn thiện layout app (header, footer, scroll, back-to-top)

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -5,16 +5,34 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import 'nprogress/nprogress.css';
-import './styles/global.css';
+import './styles/global.css'
 import ErrorPage from './pages/error.jsx';
+import RegisterPage from './pages/register.jsx';
+import HomePage from './pages/home.jsx';
+import LoginPage from './pages/login.jsx';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         errorElement: <ErrorPage />,
+        children: [
+            { index: true, element: <HomePage /> },
+        ]
+    },
+    {
+        path: "/login",
+        element: (
+            <LoginPage />
+        )
+    },
+    {
+        path: "/register",
+        element: (
+            <RegisterPage />
+        )
+    },
 
-    }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
