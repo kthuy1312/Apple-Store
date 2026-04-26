c03ec677d505b919d0b142ed89037438f29515c1
kthuy1312 <lythuy13122004@gmail.com>
2026-04-26 14:04:58 +0700

    feat(admin): thêm API lấy tất cả đơn hàng

--- be/src/routes/ad.ts
+++ be/src/routes/ad.ts
@@ -1,3 +1,4 @@
+import { getOrders } from 'controllers/admin/order-controller'
 import { disabledUser, getUsers, postUpdateUser } from 'controllers/admin/user-controller'
 import express, { Express } from 'express'
 import fileUploadMiddleware from 'src/middleware/multer'
@@ -11,6 +12,9 @@ const webRoutes = (app: Express) => {
     router.get("/admin/users", verifyToken, getUsers)
     router.put("/admin/disabled-users/:id", verifyToken, disabledUser)
     router.put("/admin/users/:userId", fileUploadMiddleware("avatar", "avatar"), verifyToken, postUpdateUser)
+
+    //order
+    router.get("/admin/orders", verifyToken, getOrders)
 
     app.use("/", router)
 }
