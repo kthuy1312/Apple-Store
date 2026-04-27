bfc013c5cff5315ade8037bb1ebb571b74187f07
kthuy1312 <lythuy13122004@gmail.com>
2026-04-27 18:14:29 +0700

    test(be): thêm unit test cho toàn bộ controller và service

---
+++ be/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }]
+    },
+    moduleNameMapper: {
+        "^config/(.*)$": "<rootDir>/src/config/$1",
+        "^services/(.*)$": "<rootDir>/src/services/$1",
+        "^controllers/(.*)$": "<rootDir>/src/controllers/$1",
+        "^src/(.*)$": "<rootDir>/src/$1",
+    }
+};
